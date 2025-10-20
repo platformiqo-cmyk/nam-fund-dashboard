9b487f7adcacbe148272987d3fe27d7d353f1b9c
Nam <platform.iqo@gmail.com>
2025-10-20 16:38:54 +0700

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ padding: "1rem 2rem", background: "#111827", color: "#fff" }}>
+      <h1 style={{ margin: 0, fontSize: "1.25rem" }}>Nam Fund Dashboard</h1>
+    </header>
+  );
+}
